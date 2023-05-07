86c3bbaec37f67e9236bea65a88b5af16772f0f5
DKlayver <davi.klayveR@live.com>
2023-05-07 16:55:25 -0300

    #13 v-for

--- aula2/src/components/Info.vue
+++ aula2/src/components/Info.vue
@@ -3,13 +3,13 @@
        
         <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
         <p v-else>Estou em busca de novas oportunidades</p>
-        <p>Utilizo as seguintes tecnologias:</p>
+        <p>Utilizo as seguintes tecnologias para back-end:</p>
         <ul>
-            <li>HTML5</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>Python</li>
-            <li>C</li>
+          <li v-for="(technology, index) in backend_technologies" v-bind:key="index">{{ technology }}</li>
+        </ul>
+        <p>Utilizo as Seguintes tecnologias para front-end</p>
+        <ul>
+            <li v-for="technology in frontend_technologies">{{ technology.language }}</li>
         </ul>
         <div>
             <button @click="showEmail">{{ textoBotao }}</button>
@@ -34,13 +34,20 @@ import Picture from './Picture.vue';
                 mostrar_email: false,
                 email: 'davi.klayver@live.com',
                 meu_link: "https://google.com",
-                textoBotao: 'Mostrar email'
+                textoBotao: 'Mostrar email',
+                backend_technologies: ['C', 'Javascript', 'Python'],
+                frontend_technologies: [
+                    {id: 1, language: 'HTML'},
+                    {id: 2, language: 'CSS'},
+                    {id: 3, language: 'Vue'}
+                ]
             }
+
         },
         methods: {
             showEmail() {
                 this.mostrar_email = !this.mostrar_email
-                if (!this.mostrar_email) {
+                if (! this.mostrar_email) {
                     this.textoBotao = 'Mostrar e-mail'
                 } else {
                     this.textoBotao = 'Ocultar e-mail'

675ca99dd1dd9c532e726965808b876de0a699f8
DKlayver <davi.klayveR@live.com>
2023-05-09 14:36:36 -0300

    #18 Emit

--- aula2/src/components/Picture.vue
+++ aula2/src/components/Picture.vue
@@ -1,13 +1,27 @@
 <template>
-    <img :src="avatar" :alt="descricao" width="240">
+    <div>
+        <img :src="avatar" :alt="descricao" width="240"> 
+        <MudarImagem @mudarImagem="trocarImagem" />
+    </div>
 </template>
 <script>
+import MudarImagem from "./MudarImagem.vue";
+
 export default {
-    name: 'Picture',
+    name: "Picture",
     data() {
         return {
-        avatar: "/img/programmer.png",
-        descricao: "Davi Klayver"
+            avatar: "/img/programmer.png",
+            descricao: "Davi Klayver"
+        };
+    },
+    components: { 
+        MudarImagem 
+    },
+    methods: {
+        trocarImagem() {
+            console.log("aaaa"),
+            this.avatar = '/img/programmer2.png';
         }
     }
 }
